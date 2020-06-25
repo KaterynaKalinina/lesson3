895b4f45a602b12419898de63716539bdab79448
Kateryna Kalinina <katya18chr@ukr.net>
2020-06-25 21:14:52 +0300

    Lesson

---
+++ script.js
@@ -0,0 +1,55 @@
+/*let num = Math.floor(Math.random() * 10);
+let guess = +prompt("Guess a number");
+
+if(unm === guess) {
+    console.log('You are right!');
+} else if (num<guess) {
+    console.log('Number is lower!');
+} else if (num>guess) {
+    console.log('Number is higher');
+} else {
+    console,log('Try again!');
+}
+
+console.log('Game over');
+*/
+/*
+let yearOfBirth = +prompt('Enter the year of your birth', '');
+let today = new Date();
+let age = today.getFullYear() - yearOfBirth;
+
+if (isNaN(age)) {
+    console.log('It is not a number');
+} else if (age >= 18) {
+    console.log('Here you go');
+} else if (age < 12) {
+    console.log('Use something else');
+} else {
+    console.log('Access denied');
+}
+*/
+
+/*let typeOfAction = prompt('Enter action type');
+
+let CREATE_ACTION = 'create';
+let READ_ACTION = 'read';
+let UPDATE_ACTION = 'update';
+let DELETE_ACTION = 'delete';
+
+switch (typeOfAction) {
+    case CREATE_ACTION:
+        console.log('Create');
+        break;
+    case READ_ACTION:
+        console.log('Read');
+        break;
+    case UPDATE_ACTION:
+        console.log('Update');
+        break;
+    case DELETE_ACTION:
+        console.log('Delete');
+        break;
+    default:
+        console.log('Default behaviour');
+}
+*/
